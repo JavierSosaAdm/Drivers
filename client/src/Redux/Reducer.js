c1b3ce40c92af2be4d84e4c90ed29678422b048d
Javier.pjs <javier.sosa.1911@gmail.com>
2023-09-26 18:23:56 -0300

    reducer parcialmente completo

--- client/src/Redux/Reducer.js
+++ client/src/Redux/Reducer.js
@@ -4,7 +4,6 @@ import {
      GET_TEAMS,
      GET_BY_NAME,
      GET_BY_ID,
-     GET_DETAIL,
      ORDER_BY_ASC,
      FILTER_BY_TEAM,
      FILTER_BY_CREATED
@@ -18,6 +17,38 @@ import {
     name: []
 };
 
-const rootReducer = () => {};
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ALL_DRIVERS:
+            return {
+                ...state,
+                allDrivers: action.payload,
+                copyAllDrivers: action.payload
+            };
+        case GET_BY_NAME:
+            return {
+                ...state,
+                allDrivers: action.payload
+            }
+        case GET_BY_ID: 
+            return {
+                ...state,
+                detail: action.payload
+            }
+        case POST_DRIVERS: 
+            return {
+                ...state,
+                allDrivers: action.payload,
+                copyAllDrivers: action.payload
+            }
+        case GET_TEAMS:
+            return {
+                ...state,
+                allTeams: action.payload
+            };
+        default:
+            return {...state};
+    }
+};
 
 export default rootReducer;
