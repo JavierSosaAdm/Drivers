e7879b90714e606f4246aa5f9fec20f97f40e420
Javier.pjs <javier.sosa.1911@gmail.com>
2023-09-26 15:21:21 -0300

    actions types y main terminados

---
+++ client/src/Redux/ActionsTypes.js
@@ -0,0 +1,11 @@
+export const POST_DRIVERS = 'POST_DRIVERS';
+export const GET_ALL_DRIVERS = 'GET_ALL_DRIVERS';
+export const GET_TEAMS = 'GET_TEAMS';
+export const GET_BY_NAME = 'GET_BY_NAME';
+export const GET_BY_ID = 'GET_BY_ID';
+export const GET_DETAIL = 'GET_DETAIL';
+export const ORDER_BY_ASC = 'ORDER_BY_ASC';
+export const FILTER_BY_TEAM = 'FILTER_BY_TEAM';
+export const FILTER_BY_CREATED = 'FILTER_BY_CREATED';
+
+
--- client/src/Redux/Reducer.js
+++ client/src/Redux/Reducer.js
@@ -1,3 +1,23 @@
+import {
+     POST_DRIVERS,
+     GET_ALL_DRIVERS,
+     GET_TEAMS,
+     GET_BY_NAME,
+     GET_BY_ID,
+     GET_DETAIL,
+     ORDER_BY_ASC,
+     FILTER_BY_TEAM,
+     FILTER_BY_CREATED
+ } from './ActionsTypes'
+
+ let initialState = { 
+    allDrivers: [], 
+    allTeams: [], 
+    detail: [], 
+    copyAllDrivers: [],
+    name: []
+};
+
 const rootReducer = () => {};
 
 export default rootReducer;
