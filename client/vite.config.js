2020caeb047cbc0d62a4a3410daf6c248516a7e2
Javier.pjs <javier.sosa.1911@gmail.com>
2023-12-11 22:12:23 -0300

    build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida para archivos construidos
+    assetsDir: '', // Carpeta para activos estáticos (como imágenes)
+    sourcemap: true, // Generar sourcemaps
+    minify: 'terser', // Minificar con Terser
+    target: 'modules' //// Formato del código de salida (ver https://vitejs.dev/guide/build.html#library-mode)
+  }
 })
