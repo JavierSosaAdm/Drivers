bf38bcd610eed311183e9bd14c728b29bd27c194
Javier.pjs <javier.sosa.1911@gmail.com>
2023-12-14 17:27:49 -0300

    server

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,7 @@ server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Origin', 'https://drivers-slw9.vercel.app'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -19,5 +20,12 @@ server.use((req, res, next) => {
   });
 
 server.use(router);
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
