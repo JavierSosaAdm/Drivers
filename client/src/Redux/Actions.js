7b3d8c5b185ef9453564ca4fe70a168173b9d7d0
Javier.pjs <javier.sosa.1911@gmail.com>
2023-12-13 21:17:29 -0300

    axios config

--- client/src/Redux/Actions.js
+++ client/src/Redux/Actions.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 // const link = 'http://localhost:3001';
-const link = 'http://drivers-production.up.railway.app'
+axios.defaults.baseURL = 'http://drivers-production.up.railway.app';
 
 export const getDrivers = () => {
     return async (dispatch) => { // es un retorno de una arrow function asincrona donde se declara una variable 
                                  // y se hace el axios get al link/drivers y se retirna un dispatch donde debe haber un 
                                  // objeto con el type: 'GETlo que sea' y el payload: response data
-        const response = await axios.get(`${link}/drivers`)
+        const response = await axios.get(`/drivers`)
         return dispatch({
             type: 'GET_ALL_DRIVERS',
             payload: response.data
@@ -17,7 +17,7 @@ export const getDrivers = () => {
 
 export const getTeams = () => {
     return async (dispatch) => {
-        const response = await axios.get(`${link}/teams`)
+        const response = await axios.get(`/teams`)
         return dispatch({
             type: 'GET_TEAMS',
             payload: response.data
@@ -28,7 +28,7 @@ export const getTeams = () => {
 export const getByName = (name) => {
     return async (dispatch) => {
         try {
-            const response = await axios.get(`${link}/search?name=${name}`)
+            const response = await axios.get(`/search?name=${name}`)
                 dispatch({
                     type: 'GET_BY_NAME',
                     payload: response.data
@@ -41,7 +41,7 @@ export const getByName = (name) => {
 
 export const getById = (id) => {
     return async (dispatch) => {
-        const response = await axios.get(`${link}/drivers/${id}`)
+        const response = await axios.get(`/drivers/${id}`)
         return dispatch({
             type: 'GET_BY_ID',
             payload: response.data
@@ -52,7 +52,7 @@ export const getById = (id) => {
  export const create = (form) => {
     try {
         return async (dispatch) => {
-            const response = await axios.post(`${link}/drivers`, form)
+            const response = await axios.post(`/drivers`, form)
             alert('el piloto fué creado correctamente');
             return dispatch({
                 type: 'POST_DRIVERS',
