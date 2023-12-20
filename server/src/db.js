d88f1c5437e2be2403cf6303f1e42533f24dd86b
Javier.pjs <javier.sosa.1911@gmail.com>
2023-12-20 20:21:56 -0300

    URL DB

--- server/src/db.js
+++ server/src/db.js
@@ -4,13 +4,19 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+//   logging: false, 
+//   native: false, 
+// });
+
+const sequelize = new Sequelize(`${URL}`, {
   logging: false, 
   native: false, 
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
